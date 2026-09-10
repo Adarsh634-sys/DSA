dec4b747ea2b53424718ca54cd817ebf8f4470ff
Adarsh kumar <adarshkumar3623@gmail.com>
2026-09-10 23:27:22 +0530

    Add files via upload

---
+++ tripletsum.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int n=5;
+    int targetsum= 9;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            for(int k=j+1; k<n; k++){
+                if(arr[i]+arr[j]+arr[k] == targetsum){
+                    cout<<arr[i]<<" "<<arr[j]<<" "<<arr[k]<<" "<<endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
